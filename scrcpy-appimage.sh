b52ad305960b5d263e94b5232b2bd8bd0fccf3c1
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-01 16:52:39 +0000

    Add files via upload

---
+++ scrcpy-appimage.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+set -ex
+
+ARCH="$(uname -m)"
+URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
+URUNTIME_LITE="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-lite-$ARCH"
+SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
+UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
+
+# Prepare AppDir
+mkdir -p ./AppDir && (
+	cd ./AppDir
+
+	# ADD LIBRARIES
+	wget --retry-connrefused --tries=30 "$SHARUN" -O ./sharun-aio
+	chmod +x ./sharun-aio
+	xvfb-run -a -- \
+		./sharun-aio l -p -v -e -s -k \
+		/usr/bin/scrcpy               \
+		/usr/bin/adb                  \
+		/usr/lib/libSDL*              \
+		/usr/lib/libGLX*              \
+		/usr/lib/libEGL*              \
+		/usr/lib/dri/*                \
+		/usr/lib/gbm/*                \
+		/usr/lib/pipewire-*/*         \
+		/usr/lib/spa-*/*/*            \
+		/usr/lib/pulseaudio/*         \
+		/usr/lib/gconv/*
+	rm -f ./sharun-aio
+
+	# Prepare sharun
+	ln ./sharun ./AppRun
+	./sharun -g
+
+	# desktop, icon, app data files
+	cp -v  /usr/share/applications/scrcpy.desktop           ./
+	cp -v  /usr/share/icons/hicolor/256x256/apps/scrcpy.png ./.DirIcon
+	cp -rv /usr/share/scrcpy                                ./share
+
+	sed -i -e 's|Exec=.*|Exec=scrcpy|g' ./scrcpy.desktop
+)
+
+VERSION="$(./AppDir/AppRun --version | awk '{print $2; exit}')"
+[ -n "$VERSION" ] && echo "$VERSION" > ~/version
+
+# MAKE APPIMAGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$URUNTIME"      -O  ./uruntime
+wget --retry-connrefused --tries=30 "$URUNTIME_LITE" -O  ./uruntime-lite
+chmod +x ./uruntime*
+
+# Add udpate info to runtime
+echo "Adding update information \"$UPINFO\" to runtime..."
+./uruntime-lite --appimage-addupdinfo "$UPINFO"
+
+echo "Generating AppImage..."
+./uruntime \
+	--appimage-mkdwarfs -f               \
+	--set-owner 0 --set-group 0          \
+	--no-history --no-create-timestamp   \
+	--compression zstd:level=22 -S26 -B8 \
+	--header uruntime-lite               \
+	-i ./AppDir                          \
+	-o ./scrcpy-"$VERSION"-anylinux-"$ARCH".AppImage
+
+# make appbundle
+UPINFO="$(echo "$UPINFO" | sed 's#.AppImage.zsync#*.AppBundle.zsync#g')"
+wget --retry-connrefused --tries=30 \
+	"https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH" -O ./pelf
+chmod +x ./pelf
+echo "Generating [dwfs]AppBundle..."
+./pelf \
+	--compression "-C zstd:level=22 -S26 -B8" \
+	--appbundle-id="scrcpy-$VERSION"          \
+	--appimage-compat                         \
+	--add-updinfo "$UPINFO"                   \
+	--add-appdir ./AppDir                     \
+	--output-to ./scrcpy-"$VERSION"-anylinux-"$ARCH".dwfs.AppBundle
+
+zsyncmake ./*.AppImage -u  ./*.AppImage
+zsyncmake ./*.AppBundle -u ./*.AppBundle
+
+mkdir -p ./dist
+mv -v ./*.AppImage*  ./dist
+mv -v ./*.AppBundle* ./dist
+
+echo "All Done!"
