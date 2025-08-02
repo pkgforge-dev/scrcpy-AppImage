54a1cf84ec3ab7b0a676de6a5659c9a7ce60bb4b
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-02 01:26:45 -0400

    build latest stable tag

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -62,12 +62,20 @@ wget --retry-connrefused --tries=30 "$OPUS_URL"   -O  ./opus.pkg.tar.zst
 pacman -U --noconfirm ./*.pkg.tar.zst
 rm -f ./*.pkg.tar.zst
 
-echo "Building mostly static scrcpy"
-git clone "https://github.com/Genymobile/scrcpy.git" ./scrcpy
+
+echo "Building mostly static scrcpy..."
+echo "---------------------------------------------------------------"
+GRON="https://raw.githubusercontent.com/xonixx/gron.awk/refs/heads/main/gron.awk"
+wget --retry-connrefused --tries=30 "$GRON" -O ./gron.awk
+chmod +x ./gron.awk
+
+LATEST_TAG=$(wget "https://api.github.com/repos/Genymobile/scrcpy/tags" -O - \
+                | ./gron.awk | awk -F'=|"' '/name/ {print $3; exit}')
+
+git clone --branch "$LATEST_TAG" --single-branch "https://github.com/Genymobile/scrcpy.git" ./scrcpy
 cd ./scrcpy
 # path needs to be updated to incldue perl
 export PATH="$PATH:/usr/bin/core_perl"
-
 # do the thing
 ./release/build_linux.sh "$ARCH"
 
