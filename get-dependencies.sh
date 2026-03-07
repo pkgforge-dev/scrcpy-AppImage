dcc6a1fb0320dac93df777f82c1abe261ec12ab0
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-07 01:35:25 -0400

    improve tag checkout

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -27,23 +27,21 @@ pacman -Syu --noconfirm \
 	xorg-server-xvfb  \
 	zsync
 
+# path needs to be updated to incldue perl
+export PATH="$PATH:/usr/bin/core_perl"
+
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 echo "Building mostly static scrcpy..."
 echo "---------------------------------------------------------------"
-GRON="https://raw.githubusercontent.com/xonixx/gron.awk/refs/heads/main/gron.awk"
-wget --retry-connrefused --tries=30 "$GRON" -O ./gron.awk
-chmod +x ./gron.awk
-
-LATEST_TAG=$(wget "https://api.github.com/repos/Genymobile/scrcpy/tags" -O - \
-                | ./gron.awk | awk -F'=|"' '/name/ {print $3; exit}')
-
-git clone --branch "$LATEST_TAG" --single-branch "https://github.com/Genymobile/scrcpy.git" ./scrcpy
+git clone "https://github.com/Genymobile/scrcpy.git" ./scrcpy
 cd ./scrcpy
-# path needs to be updated to incldue perl
-export PATH="$PATH:/usr/bin/core_perl"
+git fetch --tags origin
+TAG=$(git tag --sort=-v:refname | grep -vi 'rc\|alpha' | head -1)
+git checkout "$TAG"
+echo "$TAG" > ~/version
 # do the thing
 ./release/build_linux.sh "$ARCH"
 
