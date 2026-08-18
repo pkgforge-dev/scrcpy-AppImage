f4445f7bbf276b786719c0fccfcd8a5750088bc6
Link4Electronics <link4electronics@gmail.com>
2026-08-18 08:51:13 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 BINS_SOURCE="$PWD"/scrcpy/release/work/build-linux-"$ARCH"/dist
 UDEV="https://raw.githubusercontent.com/M0Rf30/android-udev-rules/refs/heads/main/51-android.rules"
 export ARCH
-export ADD_HOOKS="self-updater.bg.hook:udev-installer.hook"
+export ADD_HOOKS="self-updater.hook:udev-installer.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
